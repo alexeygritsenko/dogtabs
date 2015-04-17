11054269e3b20117880a94701034f6693c9fe83e
AlexeyGr <beatsspam@ya.ru>
2015-04-17 13:51:39 +0400

    coolObj9

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ res.json(coolObject);
 });
 
 app.use(express.urlencoded());
-app.post("jsoncomment", function (req, res) {
+app.use(express.bodyParser());
+app.post("/jsoncomment", function (req, res) {
   // сейчас объект сохраняется в req.body
   var newToDo = req.body;
   console.log(newToDo);
